2d404b90c499331928771ff32d43f127261cf0dc
Claire Hanson <clairecatohanson@gmail.com>
2024-01-19 11:56:02 -0600

    almost there

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,48 +1,45 @@
-import { Routes, Route } from "react-router-dom"
-import { MainMenu } from "../components/mainmenu/MainMenu"
-import { useState, useEffect } from "react"
+import { Routes, Route } from "react-router-dom";
+import { MainMenu } from "../components/mainmenu/MainMenu";
+import { useState, useEffect } from "react";
 
+import { ViewOrders } from "../components/order-details/ViewOrders";
 
-
-import { ViewOrders } from "../components/order-details/ViewOrders"
-
-import { AddEmployee } from "../components/forms/AddEmployee"
-import { EmployeeList } from "../components/employees/EmployeeList.js"
-import { UpdateEmployee } from "../components/forms/UpdateEmployee"
-import { SalesReport } from "../components/sales/SalesReport.js"
-import { AddEmployee } from "../components/forms/AddEmployee"
-
-
-
+import { AddEmployee } from "../components/forms/AddEmployee";
+import { EmployeeList } from "../components/employees/EmployeeList.js";
+import { UpdateEmployee } from "../components/forms/UpdateEmployee";
+import { SalesReport } from "../components/sales/SalesReport.js";
+import { NewOrder } from "../components/neworder/NewOrder.js";
 
 export const ApplicationViews = () => {
-    const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState({});
 
-    useEffect(() => {
-        const localNonnaUser = localStorage.getItem("nonna_user")
-        const NonnaUserObject = JSON.parse(localNonnaUser)
-        setCurrentUser(NonnaUserObject)
-    }, [])
+  useEffect(() => {
+    const localNonnaUser = localStorage.getItem("nonna_user");
+    const NonnaUserObject = JSON.parse(localNonnaUser);
+    setCurrentUser(NonnaUserObject);
+  }, []);
 
-    return (
-        <Routes>
-            <Route path="/">
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<MainMenu currentUser={currentUser} />} />
+        <Route path="orders" element={<ViewOrders />} />
+        <Route path="sales" element={<SalesReport />} />
 
-                <Route index element={<MainMenu currentUser={currentUser} />} />
-                <Route path="orders" element={<ViewOrders/>}/>
-                <Route path="sales" element={<SalesReport />} />
+        <Route index element={<MainMenu currentUser={currentUser} />} />
 
-                <Route index element={<MainMenu currentUser={currentUser}/>} />
-
-                {/* INSERT ADDITIONAL ROUTES HERE */}
-                <Route path="EmployeeList" element={<EmployeeList />} />
-                <Route path="add-employee" element={<AddEmployee />} />
-                <Route
-                    path="update-employee/:employeeId"
-                    element={<UpdateEmployee />}
-                />
-                <Route path="neworder" element={<NewOrder currentUser={currentUser}/>} />
-            </Route>
-        </Routes>
-    )
-}
+        {/* INSERT ADDITIONAL ROUTES HERE */}
+        <Route path="EmployeeList" element={<EmployeeList />} />
+        <Route path="add-employee" element={<AddEmployee />} />
+        <Route
+          path="update-employee/:employeeId"
+          element={<UpdateEmployee />}
+        />
+        <Route
+          path="neworder"
+          element={<NewOrder currentUser={currentUser} />}
+        />
+      </Route>
+    </Routes>
+  );
+};
